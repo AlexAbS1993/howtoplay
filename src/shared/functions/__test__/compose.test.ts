f9779ab681751790e841938c7b00b66e35c5da34
AlexAbS <abetovalex93@gmail.com>
2021-11-24 05:53:21 +0300

    minor Добавлен тест к compose
    Заменен reduce на reduceRight

--- src/shared/functions/__test__/compose.test.ts
+++ src/shared/functions/__test__/compose.test.ts
@@ -1,7 +1,19 @@
-export {}
+import {compose} from '../compose'
 
 describe("Функция compose работает корректно", () => {
-    test('Возвращает FC', () => {
-        
+    const sum = (num: number) => {
+        return num*10
+    }
+    const sum2 = (num: number) => {
+        return num*10
+    }
+    test('Возвращает верный результат при вычислении', () => {
+        expect(compose([sum, sum2])!(5)).toBe(500)
+    })
+    test('Возвращает функцию', () => {
+        expect(typeof compose([sum, sum2])).toBe('function')
+    })
+    test('Возвращает null, если не переданы аргументы', () => {
+        expect(compose([])(10)).toBe(null)
     })
 })
--- src/shared/functions/compose.ts
+++ src/shared/functions/compose.ts
@@ -3,8 +3,14 @@ import { FC } from "react"
 type ReactCompose = (args: Function[]) => Function
 
 export const compose: ReactCompose = (args: Function[]) => {
+    if (args.length === 0){
+        console.warn("В функцию compose не переданы аргументы")
+        return (args: any) => {
+            return null
+        }
+    }
     return (value: FC) => {
-        return args.reduce((result, fn) => {
+        return args.reduceRight((result, fn) => {
             return fn(result)
         }, value)
     }
